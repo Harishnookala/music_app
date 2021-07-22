b6e0307c1dda6375e0c6cd116c727f2c6069ec44
Harish Nookala <nookalasaiharish@gmail.com>
2021-07-22 22:49:26 +0530

    Seperation of songs and implemented music player

--- lib/main.dart
+++ lib/main.dart
@@ -1,124 +1,269 @@
-
-import 'dart:io' show Directory, FileStat, FileSystemEntity;
-
-import 'package:ext_storage/ext_storage.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:audio_manager/audio_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_audio_query/flutter_audio_query.dart';
 
-
-import 'package:permission_handler/permission_handler.dart';
-
-import 'Albums.dart';
-
+import 'package:music_app/songs.dart';
+import 'package:music_app/widget.dart';
 
 void main() {
-  runApp(MusicApp());
+  runApp(MyApp());
 }
 
-class MusicApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: "New Task",
-      debugShowCheckedModeBanner: false,
-      home: new HomePage(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  List dirPaths=[];
-  late String type;
-  late TabController _tabController;
-
-  void getFiles() async {
-
-    if (await Permission.storage.request().isGranted) {
-      print('Permission granted');
-    }
-
-  }
-
+class _MyAppState extends State<MyApp> {
   @override
   void initState() {
-    getFiles();
-    _tabController = new TabController(length: 2, vsync: this);
     super.initState();
+    setupAudio();
+  }
+
+  void setupAudio() {
+    audioManagerInstance.onEvents((events, args) {
+      switch (events) {
+        case AudioManagerEvents.start:
+          _slider = 0;
+          break;
+        case AudioManagerEvents.seekComplete:
+          _slider = audioManagerInstance.position.inMilliseconds /
+              audioManagerInstance.duration.inMilliseconds;
+          setState(() {
+
+          });
+          break;
+        case AudioManagerEvents.playstatus:
+          isPlaying = audioManagerInstance.isPlaying;
+          setState(() {
+
+          });
+          break;
+        case AudioManagerEvents.timeupdate:
+          _slider = audioManagerInstance.position.inMilliseconds /
+              audioManagerInstance.duration.inMilliseconds;
+          audioManagerInstance.updateLrc(args["position"].toString());
+          setState(() {
+
+          });
+          break;
+        case AudioManagerEvents.ended:
+          audioManagerInstance.next();
+          setState(() {
+
+          });
+          break;
+        default:
+          break;
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        toolbarHeight: 70,
-        centerTitle: true,
-        backgroundColor: Colors.deepOrangeAccent,
-        title: new Text("Music"),
-        bottom: TabBar(
-          unselectedLabelColor: Colors.blueGrey,
-          labelColor: Colors.white,
-          tabs: [
-            Container(
-              child: new Text("Songs",),
-              margin: EdgeInsets.all(12.3),
-            ),
-            Container(child: new Text("Albums"),
-              margin: EdgeInsets.all(12.3),
-            ),
-
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        drawer: Drawer(),
+        appBar: AppBar(
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  setState(() {
+                    showVol = !showVol;
+                  });
+                },
+                child: IconText(
+                    textColor: Colors.white,
+                    iconColor: Colors.white,
+                    string: "volume",
+                    iconSize: 20,
+                    iconData: Icons.volume_down),
+              ),
+            )
           ],
-          controller: _tabController,
-          indicatorColor: Colors.lightGreenAccent,
-          indicatorSize: TabBarIndicatorSize.tab,
-        ),
-        bottomOpacity: 1,
-      ),
-      body: dirPaths != null
-          ? TabBarView(
-        children: [
-          dirPaths == null
-              ? Center(
-            child: CircularProgressIndicator(),
-          )
-              : ListView.builder(
-            //if file/folder list is grabbed, then show here
-            itemCount: dirPaths.length,
-            itemBuilder: (context, index) {
-              return Card(
-                  child: ListTile(
-                    title: Container(
-                        child: Text(dirPaths.toString())),
-                    leading: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(40.0),
-                                bottomRight: Radius.circular(40.0),
-                                bottomLeft: Radius.circular(40),
-                                topLeft: Radius.circular(40)
-                            ),
-                            color: Colors.green),
-                        child: Icon(Icons.play_arrow_outlined,size: 26,)),
-
-                    onTap: (){
-
-                    },
-
-                  ));
+          elevation: 0,
+          backgroundColor: Colors.black,
+          title: showVol
+              ? Slider(
+            value: audioManagerInstance.volume ?? 0,
+            onChanged: (value) {
+              setState(() {
+                audioManagerInstance.setVolume(value, showVolume: true);
+              });
             },
-          ),
-           Albums(),
-        ],
-        controller: _tabController,
-      )
-          : Container(),
+          )
+              : Text("Music app demo"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+
+                child: FutureBuilder(
+                  future: FlutterAudioQuery()
+                      .getSongs(sortType: SongSortType.RECENT_YEAR),
+                  builder: (context, snapshot) {
+                    List<SongInfo> songInfo = snapshot.data;
+                    if (snapshot.hasData) return SongWidget(songList: songInfo);
+                    return Container(
+                      height: MediaQuery.of(context).size.height * 0.4,
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            CircularProgressIndicator(),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              "Loading....",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            bottomPanel(),
+          ],
+        ),
+      ),
     );
+  }
+
+  String _formatDuration(Duration d) {
+    if (d == null) return "--:--";
+    int minute = d.inMinutes;
+    int second = (d.inSeconds > 60) ? (d.inSeconds % 60) : d.inSeconds;
+    String format = ((minute < 10) ? "0$minute" : "$minute") +
+        ":" +
+        ((second < 10) ? "0$second" : "$second");
+    return format;
+  }
+
+  Widget songProgress(BuildContext context) {
+    var style = TextStyle(color: Colors.black);
+    return Row(
+      children: <Widget>[
+        Text(
+          _formatDuration(audioManagerInstance.position),
+          style: style,
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: SliderTheme(
+                data: SliderTheme.of(context).copyWith(
+                  trackHeight: 2,
+                  thumbColor: Colors.blueAccent,
+                  overlayColor: Colors.blue,
+                  thumbShape: RoundSliderThumbShape(
+                    disabledThumbRadius: 5,
+                    enabledThumbRadius: 5,
+                  ),
+                  overlayShape: RoundSliderOverlayShape(
+                    overlayRadius: 10,
+                  ),
+                  activeTrackColor: Colors.blueAccent,
+                  inactiveTrackColor: Colors.grey,
+                ),
+                child: Slider(
+                  value: _slider ?? 0,
+                  onChanged: (value) {
+                    setState(() {
+                      _slider = value;
+                    });
+                  },
+                  onChangeEnd: (value) {
+                    if (audioManagerInstance.duration != null) {
+                      Duration msec = Duration(
+                          milliseconds:
+                          (audioManagerInstance.duration.inMilliseconds *
+                              value)
+                              .round());
+                      audioManagerInstance.seekTo(msec);
+                    }
+                  },
+                )),
+          ),
+        ),
+        Text(
+          _formatDuration(audioManagerInstance.duration),
+          style: style,
+        ),
+      ],
+    );
+  }
+
+  Widget bottomPanel() {
+    return Column(children: <Widget>[
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: songProgress(context),
+      ),
+      Container(
+        padding: EdgeInsets.symmetric(vertical: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+
+            CircleAvatar(
+              child: Center(
+                child: IconButton(
+                    icon: Icon(
+                      Icons.skip_previous,
+                      color: Colors.white,
+                    ),
+                    onPressed: () => audioManagerInstance.previous()),
+              ),
+              backgroundColor: Colors.cyan.withOpacity(0.3),
+            ),
+
+            CircleAvatar(
+              radius: 30,
+              child: Center(
+                child: IconButton(
+                  onPressed: () async {
+                    audioManagerInstance.playOrPause();
+                  },
+                  padding: const EdgeInsets.all(0.0),
+                  icon: Icon(
+                    audioManagerInstance.isPlaying
+                        ? Icons.pause
+                        : Icons.play_arrow,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.cyan.withOpacity(0.3),
+              child: Center(
+                child: IconButton(
+                    icon: Icon(
+                      Icons.skip_next,
+                      color: Colors.white,
+                    ),
+                    onPressed: () => audioManagerInstance.next()),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ]);
   }
 }
 
-
+var audioManagerInstance = AudioManager.instance;
+bool showVol = false;
+PlayMode playMode = audioManagerInstance.playMode;
+bool isPlaying = false;
+double _slider;
